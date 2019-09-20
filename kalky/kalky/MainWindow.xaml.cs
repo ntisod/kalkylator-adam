9b7d35dcf3aa03f4f21f24c22690dcc6b2afd785
AdamTheBeast <43605392+AdamTheBeast@users.noreply.github.com>
2019-09-20 14:44:21 +0200

    miniräknare

---
+++ kalky/kalky/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MyCalculatorv1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Result_click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                result();
+            }
+            catch
+            {
+                tb.Text = "Error!";
+            }
+        }
+
+        private void result()
+        {
+            String op;
+            int iOp = 0;
+            if (tb.Text.Contains("+"))
+            {
+                iOp = tb.Text.IndexOf("+");
+            }
+            else if (tb.Text.Contains("-"))
+            {
+                iOp = tb.Text.IndexOf("-");
+            }
+            else if (tb.Text.Contains("*"))
+            {
+                iOp = tb.Text.IndexOf("*");
+            }
+            else if (tb.Text.Contains("/"))
+            {
+                iOp = tb.Text.IndexOf("/");
+            }
+          
+            op = tb.Text.Substring(iOp, 1);
+            double op1 = Convert.ToDouble(tb.Text.Substring(0, iOp));
+            double op2 = Convert.ToDouble(tb.Text.Substring(iOp + 1, tb.Text.Length - iOp - 1));
+
+            if (op == "+")
+            {
+                tb.Text += "=" + (op1 + op2);
+            }
+            else if (op == "-")
+            {
+                tb.Text += "=" + (op1 - op2);
+            }
+            else if (op == "*")
+            {
+                tb.Text += "=" + (op1 * op2);
+            }
+            else
+            {
+                tb.Text += "=" + (op1 / op2);
+            }
+        }
+
+     
+        private void Del_Click(object sender, RoutedEventArgs e)
+        {
+            tb.Text = "";
+        }
+        
+    }
+}
